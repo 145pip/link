b268ef0cef83edd6fc4def9ad2e0775ae69676c1
yoonchae <jeongyooonchae@gmail.com>
2023-03-16 17:07:15 +0900

    feat: 스테이지 공통 버튼 구현
    - 리셋버튼 svg 컴포넌트 구현 및 위치 세팅
    - 음악 버튼 svg 컴포넌트 구현 및 위치 세팅
    - 스테이지 선택 버튼 svg 컴포넌트 구현 및 위치 세팅

---
+++ src/components/buttons/commonButton.js
@@ -0,0 +1,37 @@
+import styled, { css } from "styled-components";
+
+import { ReactComponent as Reload } from "../../assets/Reload.svg";
+import { ReactComponent as Sound } from "../../assets/Sound.svg";
+import { ReactComponent as StageSelection } from "../../assets/StageSelection.svg";
+
+export default function CommonButtons() {
+  return (
+    <>
+      <StyledReload />
+      <StyledStageSelection />
+      <StyledSound />
+    </>
+  );
+}
+
+const commonStyles = css`
+  z-index: 999;
+  position: absolute;
+  top: 8%;
+`;
+
+const StyledReload = styled(Reload)`
+  right: 13%;
+  ${commonStyles};
+`;
+
+const StyledSound = styled(Sound)`
+  ${commonStyles};
+  right: 2%;
+  top: 7.5%;
+`;
+
+const StyledStageSelection = styled(StageSelection)`
+  right: 8%;
+  ${commonStyles};
+`;
