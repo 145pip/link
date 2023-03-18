0aef1716b9fa4e459ba7afa61684cbdf1740619d
Jake Jeon <j25nkh@gmail.com>
2023-03-19 03:53:39 +0900

    feat: 스테이지 0~5 기본 스크린 설정 완료
    - 스테이지 선택화면 분기를 위한 각 스테이지별 좌표 json 파일 기본 설정
    - 스테이지별 컴포넌트 기본설정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import styled from "styled-components";
 
-import StageZero from "./components/stages/StageZero";
+import Main from "./components/pages/Main";
+import GameStage from "./components/stages/GameStage";
+import StageSelection from "./components/stages/StageSelection";
 import "./reset.css";
 
 export default function App() {
+
   return (
     <AppWrapper>
-      <StageZero />
+      {screenMode === "Main" && <Main />}
+      {screenMode === "GameStage" && <GameStage />}
+      {screenMode === "StageSelection" && <StageSelection />}
     </AppWrapper>
   );
 }
--- src/components/stages/StageZero.js
+++ src/components/stages/StageZero.js
@@ -1,6 +1,6 @@
-import { OrbitControls } from "@react-three/drei";
-import { Physics } from "@react-three/cannon";
 import { Canvas } from "@react-three/fiber";
+import { Physics } from "@react-three/cannon";
+import { OrbitControls } from "@react-three/drei";
 
 import stageZeroCoordinates from "../../data/stageZeroCoordinates.json";
 import Cube from "../objects/Cube";
@@ -11,40 +11,43 @@ import AutoSnap from "../../utils/AutoSnap";
 
 export default function StageZero() {
   return (
-    <Canvas
-      camera={{
-        position: [15, 15, 15],
-        near: 0.01,
-        far: 1000,
-        fov: 50,
-        zoom: 1,
-      }}
-    >
-      <ambientLight />
-      <pointLight position={[-10, -10, -10]} />
-      <spotLight angle={0.25} penumbra={0.5} position={[10, 10, 5]} />
-      <Axis />
-      <Physics>
-        {stageZeroCoordinates.cubes.positions.map((position, index) => (
-          <Cube key={`stage0-cube-${index + 1}`} position={position} />
-        ))}
-        {stageZeroCoordinates.linkEdges.map((linkEdge, index) => (
-          <LinkEdge key={`stage0-link-edge-${index + 1}`} linkEdge={linkEdge} />
-        ))}
-        {stageZeroCoordinates.linkEdges.map((linkEdge, index) => (
-          <AutoSnap
-            key={`stage0-auto-snap-${index + 1}`}
-            linkSensitivity={0.05}
-            linkEdge={linkEdge}
-          />
-        ))}
-      </Physics>
-      <PlayerObject
-        position={[0, 1, 1.5]}
-        rotation={[0, -Math.PI / 2, 0]}
-        scale={0.5}
-      />
-      <OrbitControls />
-    </Canvas>
+      <Canvas
+        camera={{
+          position: [15, 15, 15],
+          near: 0.01,
+          far: 1000,
+          fov: 50,
+          zoom: 1,
+        }}
+      >
+        <ambientLight />
+        <pointLight position={[-10, -10, -10]} />
+        <spotLight angle={0.25} penumbra={0.5} position={[10, 10, 5]} />
+        <Axis />
+        <Physics>
+          {stageZeroCoordinates.cubes.positions.map((position, index) => (
+            <Cube key={`stage0-cube-${index + 1}`} position={position} />
+          ))}
+          {stageZeroCoordinates.linkEdges.map((linkEdge, index) => (
+            <LinkEdge
+              key={`stage0-link-edge-${index + 1}`}
+              linkEdge={linkEdge}
+            />
+          ))}
+          {stageZeroCoordinates.linkEdges.map((linkEdge, index) => (
+            <AutoSnap
+              key={`stage0-auto-snap-${index + 1}`}
+              linkSensitivity={0.05}
+              linkEdge={linkEdge}
+            />
+          ))}
+        </Physics>
+        <PlayerObject
+          position={[0, 1, 1.5]}
+          rotation={[0, -Math.PI / 2, 0]}
+          scale={0.5}
+        />
+        <OrbitControls />
+      </Canvas>
   );
 }
