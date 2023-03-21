4804b3971b8579f9cbd0572b1b2ee3076ad69584
Jake Jeon <j25nkh@gmail.com>
2023-03-22 01:32:00 +0900

    feat: 플레이어 오브젝트 애니메이션 구현
    - 스테이지 로드시 플레이어 오브젝트 standing 형태로 구현
    - 키보드 입력시 플레이어 오브젝트 walking animation 구현
    - 플레이어 오브젝트의 헤드 방향에 따른 모션컨트롤 기능 구현

--- src/components/stages/StageZero.js
+++ src/components/stages/StageZero.js
@@ -1,16 +1,68 @@
+import { useState, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Physics } from "@react-three/cannon";
 import { OrbitControls } from "@react-three/drei";
 
 import stageZeroCoordinates from "../../data/stageZeroCoordinates.json";
 import Cube from "../objects/Cube";
-import Axis from "../objects/Axis";
-import LinkEdge from "../objects/LinkEdge";
-import PlayerObject from "../objects/PlayerObject";
 import AutoSnap from "../../utils/AutoSnap";
 import SkipMenu from "../menus/SkipMenu";
+import PlayerObject from "../objects/PlayerObject";
 
 export default function StageZero() {
+  const movementIncrement = 0.05;
+  const rotationIncrement = (5 * Math.PI) / 180;
+  const [playerObjectPosition, setPlayerObjectPosition] = useState([0, 1, 1]);
+  const [playerObjectRotation, setPlayerObjectRotation] = useState([0, 0, 0]);
+  const [isMoving, setIsMoving] = useState(false);
+
+  useEffect(() => {
+    const handleKeyDown = event => {
+      if (event.code === "ArrowUp") {
+        setIsMoving(true);
+        for (let i = 0; i < 20; i++) {
+          setTimeout(() => {
+            setPlayerObjectPosition(position => [
+              position[0],
+              position[1],
+              position[2] + movementIncrement,
+            ]);
+          }, i * 50);
+        }
+      } else if (event.code === "ArrowDown") {
+        setIsMoving(false);
+      } else if (event.code === "ArrowRight") {
+        setIsMoving(false);
+        for (let i = 0; i < 18; i++) {
+          setTimeout(() => {
+            setPlayerObjectRotation(position => [
+              position[0],
+              position[1] - rotationIncrement,
+              position[2],
+            ]);
+          }, i * 25);
+        }
+      } else if (event.code === "ArrowLeft") {
+        setIsMoving(false);
+        for (let i = 0; i < 18; i++) {
+          setTimeout(() => {
+            setPlayerObjectRotation(position => [
+              position[0],
+              position[1] + rotationIncrement,
+              position[2],
+            ]);
+          }, i * 25);
+        }
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [playerObjectPosition]);
+
   return (
     <>
       <Canvas
@@ -25,29 +77,26 @@ export default function StageZero() {
         <ambientLight />
         <pointLight position={[-10, -10, -10]} />
         <spotLight angle={0.25} penumbra={0.5} position={[10, 10, 5]} />
-        <Axis />
         <Physics>
           {stageZeroCoordinates.cubes.positions.map(position => (
-            <Cube key={`stage0-cube-${position}`} position={position} />
-          ))}
-          {stageZeroCoordinates.linkEdges.map(linkEdge => (
-            <LinkEdge
-              key={`stage0-link-edge-${linkEdge}`}
-              linkEdge={linkEdge}
+            <Cube
+              key={`stage0-cube-${JSON.stringify(position)}`}
+              position={position}
             />
           ))}
           {stageZeroCoordinates.linkEdges.map(linkEdge => (
             <AutoSnap
-              key={`stage0-auto-snap-${linkEdge}`}
+              key={`stage0-auto-snap-${JSON.stringify(linkEdge)}`}
               linkSensitivity={0.05}
               linkEdge={linkEdge}
             />
           ))}
         </Physics>
         <PlayerObject
-          position={[0, 1, 1.5]}
-          rotation={[0, -Math.PI / 2, 0]}
-          scale={0.5}
+          position={playerObjectPosition}
+          rotation={playerObjectRotation}
+          scale={2}
+          isMoving={isMoving}
         />
         <OrbitControls />
       </Canvas>
