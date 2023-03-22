0e441c0628552ee5e9da8321a1d1bbdf72dfc77e
xollee <devleexol@gmail.com>
2023-03-22 20:11:38 +0900

    feat: edgeLinkSlice 구현, redux-logger 설치, ESLint 수정
    - 개발 디펜던시에 redux-logger 추가
    - ESLint import/no-extraneous-dependencies 룰 추가
    - 리덕스 스토어에 로거 설정
    - edgeLinkSlice 구현

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+
 import stageLevelReducer from "./stageLevelSlice";
 import screenModeSliceReducer from "./screenModeSlice";
+import edgeLinkSliceReducer from "./edgeLinkSlice";
 
 const store = configureStore({
   reducer: {
     stageLevel: stageLevelReducer,
     screenMode: screenModeSliceReducer,
+    edgeLink: edgeLinkSliceReducer,
   },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
