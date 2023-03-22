7994fa46f2a424d63a6fbdc54d08be1a30d6ad03
xollee <devleexol@gmail.com>
2023-03-22 21:13:07 +0900

    feat: 오토 스냅에 throttle 구현
    - 기존 코드에 있던 useFrame을 throttled 함수에서 돌아가도록 구현

--- src/utils/AutoSnap.js
+++ src/utils/AutoSnap.js
@@ -1,23 +1,48 @@
+import { useRef } from "react";
 import { useThree, useFrame } from "@react-three/fiber";
 import { OrthographicCamera } from "@react-three/drei";
+import { useDispatch } from "react-redux";
 import PropTypes from "prop-types";
 
 import LinkAnglesCalculator from "./LinkAngleCalculator";
+import { setIsLink, setLinkEdge } from "../redux/edgeLinkSlice";
+
+const throttled = (callback, delay) => {
+  const lastRun = useRef(Date.now());
+
+  useFrame(() => {
+    if (Date.now() - lastRun.current >= delay) {
+      callback();
+      lastRun.current = Date.now();
+    }
+  });
+};
 
 export default function AutoSnap({ linkSensitivity, linkEdge }) {
   const { camera } = useThree();
+  const dispatch = useDispatch();
+  const prevInRange = useRef(null);
   const linkAngles = LinkAnglesCalculator(linkEdge);
 
-  useFrame(() => {
-    if (
+  const checkCameraRotation = () => {
+    const inRange =
       camera.rotation.x > linkAngles.x - linkSensitivity &&
       camera.rotation.x < linkAngles.x + linkSensitivity &&
       camera.rotation.y > linkAngles.y - linkSensitivity &&
-      camera.rotation.y < linkAngles.y + linkSensitivity
-    ) {
+      camera.rotation.y < linkAngles.y + linkSensitivity;
+
+    if (inRange) {
       camera.rotation.set(linkAngles.x, linkAngles.y, camera.rotation.z);
     }
-  });
+
+    if (prevInRange.current !== inRange) {
+      prevInRange.current = inRange;
+      dispatch(setIsLink(inRange));
+      dispatch(setLinkEdge(linkEdge));
+    }
+  };
+
+  throttled(checkCameraRotation, 10);
 
   return (
     <OrthographicCamera
